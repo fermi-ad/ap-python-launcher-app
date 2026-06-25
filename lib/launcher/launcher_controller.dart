bef8817fc2f89b1da0f1fdb293eca758ed45728b
Connor Howington <chowingt@fnal.gov>
2026-06-25 12:31:13 -0500

    Suppress misleading lint

--- lib/launcher/launcher_controller.dart
+++ lib/launcher/launcher_controller.dart
@@ -27,6 +27,10 @@ class LauncherController {
     this.pollInterval = const Duration(seconds: 2),
     this.endPollInterval = const Duration(seconds: 2),
     this.endTimeout = const Duration(seconds: 60),
+    // The analyzer incorrectly thinks that `Config.apiBaseUrl` doesn't do
+    // anything just because its default of an empty string matches the
+    // `baseUrl` param's default.
+    // ignore: avoid_redundant_argument_values
   }) : _api = apiService ?? api.HttpApiService(baseUrl: Config.apiBaseUrl),
        _jobs = jobStore ?? jobs.JobStore();
   final api.ApiService _api;
